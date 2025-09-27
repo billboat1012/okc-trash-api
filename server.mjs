7f377d08f1cdc512a0e6d6f6c35bfe9a736cec67
Sython Ladsavong <kzvikt666@gmail.com>
2025-09-27 07:16:22 +0700

    render version 1

--- server.mjs
+++ server.mjs
@@ -1,15 +1,13 @@
 import express from "express";
 import pickupsRouter from "./routes/pickups.mjs";
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.get("/ping", (req, res) => {
   res.json({ status: "ok", time: new Date().toISOString() });
 });
 
 app.use("/api/pickups", pickupsRouter);
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
